f36458608577e9631d7505b9c9d687ad39573265
Rob Benney <rob.benney@gmail.com>
2026-02-27 10:47:27 -0500

    setup routes and navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 import Home from "./assets/pages/Home";
+import Favorites from "./assets/pages/Favorites"
 
 function App() {
 
   return (
-    <>
-     <Home />
-    </>
-  )
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App
